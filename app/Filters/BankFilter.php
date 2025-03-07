77ac0924028eafd4a6c6a88b0745f839a3460d9e
caleb <caleb.meteor@gmail.com>
2025-03-07 10:30:32 +0800

    银行卡，订单支持搜索

--- app/Filters/BankFilter.php
+++ app/Filters/BankFilter.php
@@ -6,5 +6,8 @@ use Caleb\Practice\QueryFilter;
 
 class BankFilter extends QueryFilter
 {
-
+    public function cardNumber($cardNumber)
+    {
+        return $this->query->where('card_number', 'like', "%$cardNumber%");
+    }
 }
--- app/Filters/OrderFilter.php
+++ app/Filters/OrderFilter.php
@@ -10,4 +10,19 @@ class OrderFilter extends QueryFilter
     {
         return $this->query->where('customer_id', $customerId);
     }
+
+    public function orderNumber($orderNumber)
+    {
+        return $this->query->whereLike('order_number', '%'.$orderNumber.'%');
+    }
+
+    public function status($status)
+    {
+        return $this->query->where('status', $status);
+    }
+
+    public function deliveryStatus($deliveryStatus)
+    {
+        return $this->query->where('delivery_status', $deliveryStatus);
+    }
 }
